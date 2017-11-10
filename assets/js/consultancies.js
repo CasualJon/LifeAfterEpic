17331dd850c59bc59cb366c50d83d7abe926763d
CasualJon <jon@joncyrus.com>
2017-11-10 16:13:07 -0600

    Updating first pass at consulting tables, removing json files

--- assets/js/consultancies.js
+++ assets/js/consultancies.js
@@ -1,19 +1,72 @@
-//Interacts with consultancies.json to populate tables
-//on lifeafterepic.com/resourcepgs/consulting.html
+//Seems like I might not be able to query against a JSON file from gh-pages
+//For now, storing the data needed to build table within this js file
+//... yay workarounds.
 
-console.log("Testing JS triggering on page");
+var firms = [
+  { name: "BlueTree Network",
+    website: "https://www.bluetreenetwork.com/",
+    logo: "../assets/img/consulting/bluetreenetwork.jpg",
+    twitter: "https://twitter.com/BluetreeNetwork",
+    linkedin: "https://www.linkedin.com/company/2663341/",
+    confirmed: true
+  },
 
-var xhttpConf = new XMLHttpRequest();
-xhttpConf.onreadystatechange = function() {
-  console.log("this.readyState: " + this.readyState + " && this.status: " + this.status);
-    if (this.readyState == 4 && this.status == 200) {
-      // Typical action to be performed when the document is ready:
-      //document.getElementById("confirmedFirms").innerHTML = xhttp.responseText;
+  { name: "Nordic Consulting Partners",
+    website: "https://www.nordicwi.com/",
+    logo: "../assets/img/consulting/nordic.jpg",
+    twitter: "https://twitter.com/nordicwi",
+    linkedin: "https://www.linkedin.com/company/819886/",
+    confirmed: true
+  },
 
-      console.log("In if(this.readyState==4 && this.status==200)");
-      var resonse = JSON.parse(xhttpConf.responseText);
-    }
-};
-console.log("Out of .onsreadystatechange function");
-xhttpConf.open("GET", "./consultancies.json", true);
-xhttpConf.send();
+  { name: "The HCI Group",
+    website: "https://www.thehcigroup.com/",
+    logo: "../assets/img/consulting/hcigroup.jpg",
+    twitter: "https://twitter.com/TheHCIGroup",
+    linkedin: "https://www.linkedin.com/company/954937/",
+    confirmed: true
+  },
+
+  { name: "Huron Consulting Group",
+    website: "https://www.huronconsultinggroup.com/",
+    logo: "../assets/img/consulting/huron.jpg",
+    twitter: null,
+    linkedin: "https://www.linkedin.com/company/5751/",
+    confirmed: true
+  }
+];
+
+var confOutput = "";
+for (var i = 0; i < firms.length; i++) {
+  confOutput += "<tr><td scope=\"row\">";
+  confOutput += "<img scr=\"" + firms[i].logo +"\" alt=\"" + firms[i].name +"\" class=\"img-responsive\"/>";
+  confOutput += "</td><td>";
+  confOutput += firms[i].name;
+  confOutput += "</td><td>";
+
+  if (firms[i].website != null) {
+    confOutput += "<a href="+ firms[i].website +" target=\"_blank\"><i class=\"fa fa-laptop\"></i></a>";
+  }
+  else {
+    confOutput += " ";
+  }
+  confOutput += "</td><td>";
+
+  if (firms[i].linkedin != null) {
+    confOutput += "<a href="+ firms[i].linkedin +" target=\"_blank\"><i class=\"fa fa-linkedin\"></i></a>";
+  }
+  else {
+    confOutput += " ";
+  }
+  confOutput += "</td><td>";
+
+  if (firms[i].twitter != null) {
+    confOutput += "<a href="+ firms[i].twitter +" target=\"_blank\"><i class=\"fa fa-twitter\"></i></a>";
+  }
+  else {
+    confOutput += " ";
+  }
+  confOutput += "</td></tr>";
+}
+document.getElementById('confirmedFirms').innerHTML = confOutput;
+console.log(confOutput);
