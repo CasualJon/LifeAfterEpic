bba59be4acf2963067352f0c5f9ca02d9c76fcf1
CasualJon <jon@joncyrus.com>
2017-11-10 13:59:37 -0600

    More JS debugging code

--- assets/js/consultancies.js
+++ assets/js/consultancies.js
@@ -6,11 +6,13 @@ console.log("Testing JS triggering on page");
 var xhttpConf = new XMLHttpRequest();
 xhttpConf.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-       // Typical action to be performed when the document is ready:
-       //document.getElementById("confirmedFirms").innerHTML = xhttp.responseText;
+      // Typical action to be performed when the document is ready:
+      //document.getElementById("confirmedFirms").innerHTML = xhttp.responseText;
 
-       console.log(xhttpConf.responseText);
+      console.log("In if(this.readyState==4 && this.status==200)");
+      var resonse = JSON.parse(xhttpConf.responseText);
     }
 };
+console.log("Out of .onsreadystatechange function");
 xhttpConf.open("GET", "../consultancies.json", true);
 xhttpConf.send();
