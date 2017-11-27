d8b5e0715014a10d39df38582719d1ff10ba99f4
CasualJon <jon@joncyrus.com>
2017-11-26 18:29:11 -0600

    adding to non-compete form

---
+++ assets/js/noncompete.js
@@ -0,0 +1,28 @@
+//js for EpicNonCompete.com page, hosted within LifeAfterEpic.com
+
+//Fetching HTML elements in variables by ID
+var ENCForm = document.getElementById("Epic_Non_Compete_Form");
+//Create new element form
+var createForm = document.createElement('form');
+
+//Setting action attribute on form
+createForm.setAttribute("action", "");
+//Setting method attribute on form
+createForm.setAttribute("method", "post");
+
+ENCForm.appendChild(createForm);
+
+//Left alignment of form
+// var leftAlign = document.createElement("align=\"left\"");
+// createForm.appendChild(leftAlign);
+
+//Heading of form
+var heading = document.createElement('h3');
+heading.innerHTML = "Your Epic Information: ";
+createForm.appendChild(heading);
+
+//Insert horizontal row/line following heading
+var line = document.crateElement('hr');
+createForm.appendChild(line);
+var linebreak = document.createElement('br');
+crateForm.appendChild(linebreak);
