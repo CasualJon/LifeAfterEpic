7659db2269dfbef55c02077830c0baf71b8dfcaa
CasualJon <jon@joncyrus.com>
2018-03-27 11:16:43 -0500

    adding Chris Butsch profile, adding all profiles page, adjusting folder structure to better accommodate future profiles

---
+++ assets/js/carouselCycle.js
@@ -0,0 +1,8 @@
+// Carousel Auto-Cycle
+  $(document).ready(function() {
+    $('.carousel').carousel({
+      interval: 6000
+    })
+  });
+
+//https://bootsnipp.com/snippets/featured/responsive-moving-box-carousel
